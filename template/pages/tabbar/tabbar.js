7e566806ccbacc2f2d4bf6af4443633781bdba59
oarnged <1208917130@qq.com>
2020-06-25 10:04:45 +0800

    开始设置的模板

---
+++ template/pages/tabbar/tabbar.js
@@ -0,0 +1,77 @@
+// pages/tabbar/tabbar.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    PageCur: 'share'
+  },
+
+  //导航栏点击事件
+  NavChange(e) {
+    this.setData({
+      PageCur: e.currentTarget.dataset.cur
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: 'ColorUI-高颜值的小程序UI组件库',
+      imageUrl: '/images/share.jpg',
+      path: '/pages/index/index'
+    }
+  }
+})
